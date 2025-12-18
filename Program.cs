d99ad7de15a16c37ec29f00ced7088d269337106
unknown <unai.arevalo.7e8@itb.cat>
2025-12-18 10:40:27 +0100

    add Program.cs

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RefactoringOldCode
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Haro!!");
+        }
+    }
+}

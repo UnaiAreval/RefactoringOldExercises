01f4278af5fedb71cb1d02eff4075d2f41b1fd25
unknown <unai.arevalo.7e8@itb.cat>
2025-12-18 10:36:56 +0100

    feat: exercise four refactored added

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace RefactoringOldCode
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Random random = new Random();
+            int secretNum = 0;
+            int dorsNumber;
+            bool validIntEnter = true;
+
+            do
+            {
+                Console.WriteLine(validIntEnter ? "Introdueix un número secret que sigui més gran que zero:" : "El valor introduit no es un nombre\nReintrodueix el número:");
+                
+                validIntEnter = Int32.TryParse(Console.ReadLine(), out secretNum);
+            } while (!validIntEnter || secretNum <= 0);
+
+            dorsNumber = random.Next(5, 16);
+            Console.WriteLine("Hi ha {0} portes", dorsNumber);
+
+            Console.Write("Pots obrir les portes: ");
+            for (int i = 1; i < dorsNumber + 1; i++)
+            {
+                if (i % secretNum == 0)
+                {
+                    Console.Write("{0}, ", i);
+                }
+            }
+
+        }
+    }
+}
